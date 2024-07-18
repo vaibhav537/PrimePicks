3b8dfe9d6c8f926ea0ffb3ee287800a4c825c96e
vaibhav537 <vaibhavmali537@gmail.com>
2024-07-18 20:02:00 +0530

    improvements at signup page

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -20,7 +20,7 @@ const Page = () => {
             width={150}
           />
         </Link>
-        <div className="w-full bg-white rounded-lg shadow-lg md:mt-0 sm:max-w-md xl:p-0">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
               Create Your Account
@@ -39,9 +39,37 @@ const Page = () => {
                   name="email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="bg-gray-50 border border-e-gray-300 text-gray-300 sm:text-sm rounded-lg "
+                  className="bg-gray-50 border border-e-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5 w-full block focus:ring-orange-500 outline-none focus:border-orange-600"
+                  autoComplete="off"
+                  placeholder="name@company.com"
                 />
               </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Your Email
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="bg-gray-50 border border-e-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5 w-full block focus:ring-orange-500 outline-none focus:border-orange-600"
+                  autoComplete="off"
+                  placeholder="*************"
+                />
+              </div>
+              <button
+                className="w-full text-white bg-orange-400 hover:bg-orange-500 font-medium text-sm py-2.5 rounded-lg px-5 text-center"
+                onClick={handleSignup}
+              >
+                {" "}
+                Create an Account{" "}
+              </button>
+              <p className="text-sm font-light text-gray-500 ">Already have an account? <Link className="font-medium text-blue-600 hover:underline" href="/login">Login</Link></p>
             </div>
           </div>
         </div>
