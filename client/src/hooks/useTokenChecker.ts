fb7071890b117a030b000647789b056a0153046a
vaibhav537 <vaibhavmali537@gmail.com>
2024-07-26 22:26:26 +0530

    auth completed

--- client/src/hooks/useTokenChecker.ts
+++ client/src/hooks/useTokenChecker.ts
@@ -4,27 +4,31 @@ import { useRouter } from "next/navigation";
 import jwt from "jsonwebtoken";
 import { verifyToken, isTokenExpired } from "../lib/utils/verifyToken";
 
-const useTokenChecker = (token: string): 
-{
-  isValid: boolean; tokenData: jwt.JwtPayload | null } => {
+const useTokenChecker = (
+  token: string
+): { isValid: boolean; tokenData: jwt.JwtPayload | null } => {
   const [isTokenValid, setIsTokenValid] = useState<boolean>(true);
   const [tokenData, setTokenData] = useState<jwt.JwtPayload | null>(null);
   const router = useRouter();
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
+    const checkToken = () => {
       const decodedToken = verifyToken(token);
       const tokenExpired = isTokenExpired(token);
+
       if (decodedToken && !tokenExpired) {
         setIsTokenValid(true);
         setTokenData(decodedToken);
       } else {
         setIsTokenValid(false);
         setTokenData(null);
+        localStorage.removeItem("accessToken");
+        router.push("/");
       }
-      localStorage.removeItem("accessToken");
-      router.push("/");
-    }, 1000);
+    };
+
+    const intervalId = setInterval(checkToken, 1000);
+    checkToken(); // Check token immediately on mount
 
     return () => clearInterval(intervalId);
   }, [token, router]);
